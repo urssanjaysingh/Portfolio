8fec2d95075660fa3ba36339de85dc908ee69636
urssanjaysingh <sanjaysingh26112000@gmail.com>
2023-08-12 08:07:08 +0530

    Changing Pictures

--- src/component/LandingPage.js
+++ src/component/LandingPage.js
@@ -23,11 +23,12 @@ const LandingPage = () => {
                             </div>
                         </div>
                     </section>
+
                     <section className="portfolio-block photography">
                         <div className="container">
                             <div className="row g-0">
                                 <div className="col-md-6 col-lg-4 item zoom-on-hover">
-                                    <Link to="#"><img className="img-fluid image" src="img/main/html.jpg" alt="HTML" /></Link>
+                                    <Link to="#"><img className="img-fluid image" src="img/main/mongodb" alt="MongoDB" /></Link>
                                 </div>
                                 <div className="col-md-6 col-lg-4 item zoom-on-hover">
                                     <Link to="#"><img className="img-fluid image" src="img/main/mern.jpg" alt="MERN" /></Link>
